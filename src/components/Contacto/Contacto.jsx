616368cb11469f7887e258800e97946414fda0b4
Miguel Miguez <138310942+MiguelMiguez@users.noreply.github.com>
2025-04-07 22:37:12 -0300

    Update

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,87 +1,87 @@
-import React, { useState } from 'react';
-import './Contacto.css';
-import Modal from '../Modal/Modal';
+import React, { useState } from "react";
+import "./Contacto.css";
 
 const Contacto = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    surname: '',
-    email: '',
-    number: ''
+    name: "",
+    surname: "",
+    email: "",
+    number: "",
   });
-
-  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const handleChange = (e) => {
     const { id, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [id]: value
+      [id]: value,
     }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('/php/contact.php', {
-      method: 'POST',
+    const response = await fetch("/php/contact.php", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: new URLSearchParams(formData)
+      body: new URLSearchParams(formData),
     });
     const result = await response.json();
-    if (result.status === 'success') {
+    if (result.status === "success") {
       setIsModalVisible(true);
       setTimeout(() => {
         setIsModalVisible(false);
       }, 5000);
     } else {
-      alert('Error al enviar el correo');
+      alert("Error al enviar el correo");
     }
   };
 
   return (
-    <div className='Contacto'>
-      <h2 className='TittleForm'>contactanos</h2>
+    <div className="Contacto">
+      <h2 className="TittleForm">contactanos</h2>
       <form onSubmit={handleSubmit}>
-        <h3 className='SubTittleForm'>Nosotros nos <span>encargamos</span> de todo!</h3>
-        <div className='FormGroup'>
+        <h3 className="SubTittleForm">
+          Nosotros nos <span>encargamos</span> de todo!
+        </h3>
+        <div className="FormGroup">
           <input
-            className='ContactoForm'
+            className="ContactoForm"
             type="text"
-            id='name'
-            placeholder='Nombre'
+            id="name"
+            placeholder="Nombre"
             value={formData.name}
             onChange={handleChange}
           />
           <input
-            className='ContactoForm'
+            className="ContactoForm"
             type="text"
-            id='surname'
-            placeholder='Apellido'
+            id="surname"
+            placeholder="Apellido"
             value={formData.surname}
             onChange={handleChange}
           />
           <input
-            className='ContactoForm'
+            className="ContactoForm"
             type="email"
-            id='email'
-            placeholder='ejemplo@email.com'
+            id="email"
+            placeholder="ejemplo@email.com"
             value={formData.email}
             onChange={handleChange}
           />
           <input
-            className='ContactoForm'
+            className="ContactoForm"
             type="number"
-            id='number'
-            placeholder='telefono'
+            id="number"
+            placeholder="telefono"
             value={formData.number}
             onChange={handleChange}
           />
         </div>
-        <button className='ButtonForm' type="submit">ENVIAR</button>
+        <button className="ButtonForm" type="submit">
+          ENVIAR
+        </button>
       </form>
-      <Modal isVisible={isModalVisible} />
     </div>
   );
 };
