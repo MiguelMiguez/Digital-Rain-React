1e8d6cde513522262790d1c68a40e40b6df247fd
Miguel Miguez <138310942+MiguelMiguez@users.noreply.github.com>
2025-04-08 21:58:06 -0300

    update

--- src/components/Container/ContainerAll.jsx
+++ src/components/Container/ContainerAll.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import './ContainerAll.css'
-import Home from '../Home/Home';
-import Navbar from '../Navbar/Navbar';
-import Nosotros from '../Nosotros/Nosotros';
-import Elegirnos from '../Elegrinos/Elegirnos';
-import Servicios from '../Servicios/Servicios';
-import Contacto from '../Contacto/Contacto';
-import Footer from '../Footer/Footer';
-
+import React from "react";
+import "./ContainerAll.css";
+import Home from "../Home/Home";
+import Navbar from "../Navbar/Navbar";
+import Nosotros from "../Nosotros/Nosotros";
+import Elegirnos from "../Elegrinos/Elegirnos";
+import Servicios from "../Servicios/Servicios";
+import Contacto from "../Contacto/Contacto";
+import Footer from "../Footer/Footer";
+import Portfolio from "../Portfolio/Portfolio";
 
 const ContainerAll = () => {
   return (
-    <div className='ContainerAll'>
+    <div className="ContainerAll">
       <Navbar />
-      <div id='home'><Home /></div>
-      <div id='nosotros'><Nosotros /></div>
-      <div id='elegirnos'><Elegirnos /></div>
-      <div id='servicios'><Servicios /></div>
-      <div id='contacto'><Contacto /></div>
+      <div id="home">
+        <Home />
+      </div>
+      <div id="nosotros">
+        <Nosotros />
+      </div>
+      <div id="elegirnos">
+        <Elegirnos />
+      </div>
+      <div id="servicios">
+        <Servicios />
+      </div>
+      <div id="Portfolio">
+        <Portfolio />
+      </div>
+      <div id="contacto">
+        <Contacto />
+      </div>
       <Footer />
     </div>
   );
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Portfolio.css";
 
 const Portfolio = () => {
   return (
-    <div className='Portfolio'>
-        
+    <div className="Portfolio">
+      <div class="snap-x ...">
+        <div class="snap-start scroll-ml-6 ...">
+          <img src="/Portfolio/Campo fertil.png" />
+        </div>
+        <div class="snap-start scroll-ml-6 ...">
+          <img src="/Portfolio/Campo fertil.png" />
+        </div>
+        <div class="snap-start scroll-ml-6 ...">
+          <img src="/Portfolio/Campo fertil.png" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
